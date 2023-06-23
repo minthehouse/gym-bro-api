068f6ff6b71b23276b2fe8fa5057ea33fd31dc2a
minthehouse <min.navy.lee@gmail.com>
2023-06-23 18:09:36 -0400

    remove and add columns to users table to store data properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_214359) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_220832) do
   create_table "exercise_types", force: :cascade do |t|
     t.string "name"
     t.string "muscle_group"
@@ -42,13 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_214359) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
     t.string "email"
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "height"
+    t.integer "weight"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
