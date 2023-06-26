bc24b4db3ca2f2b4979fa2c75b85876e39161928
minthehouse <min.navy.lee@gmail.com>
2023-06-26 19:00:01 -0400

    rm create_table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_225641) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_220832) do
   create_table "exercise_types", force: :cascade do |t|
     t.string "name"
     t.string "muscle_group"
@@ -30,40 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_225641) do
     t.index ["workout_id"], name: "index_exercises_on_workout_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.text "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-  end
-
   create_table "workouts", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "start_at"
     t.datetime "end_at"
     t.integer "duration_in_minutes"
-    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "exercises", "workouts"
-  add_foreign_key "workouts", "users"
 end
