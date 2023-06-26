3495e4329423d3bf2568914f066cc854ca4f11b4
minthehouse <min.navy.lee@gmail.com>
2023-06-26 18:58:28 -0400

    rm another migrate about Users table

--- db/migrate/20230623220802_remove_columns_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :nickname, :string
-    remove_column :users, :image, :string
-  end
-end
