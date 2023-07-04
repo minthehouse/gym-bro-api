6bda05e888989a0d3a569989f4a059c62e1741ee
minthehouse <min.navy.lee@gmail.com>
2023-07-03 20:43:03 -0400

    add token to response after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < DeviseTokenAuth::SessionsController
+
+  def create
+    super do
+      render json: { user: current_user, 
+                     token: @token }.to_json and return
+    end
+  end
+
   private
 
   def resource_params
     params.require(:session).permit(:email, :password)
   end
+
+
 end
